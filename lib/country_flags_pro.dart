5b4735cf54f1e179359b447bfe2636df119eca8c
Sanjarbek <sanjarbekf8@gmail.com>
2025-01-31 19:37:25 +0500

    READ.me

--- lib/country_flags_pro.dart
+++ lib/country_flags_pro.dart
@@ -1,5 +1,55 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CountryFlagsPro {
+  // To'g'ri davlat kodlari ro'yxati
+  static const List<String> _validCountryCodes = [
+    'af', 'ag', 'ai', 'al', 'am', 'ao', 'aq', 'ar', 'as', 'at', 'au', 'aw', 'ax', 'az',
+    'ba', 'bb', 'bd', 'be', 'bf', 'bg', 'bh', 'bi', 'bj', 'bl', 'bm', 'bn', 'bo', 'bq',
+    'br', 'bs', 'bt', 'bv', 'bw', 'by', 'bz', 'ca', 'cc', 'cd', 'cf', 'cg', 'ch', 'ci', 'ck',
+    'cl', 'cm', 'cn', 'co', 'cr', 'cu', 'cv', 'cw', 'cx', 'cy', 'cz', 'de', 'dj', 'dk', 'dm',
+    'do', 'dz', 'ec', 'ee', 'eg', 'eh', 'er', 'es', 'et', 'eu', 'fi', 'fj', 'fk', 'fm', 'fo',
+    'fr', 'ga', 'gb', 'gb-eng', 'gb-nir', 'gb-sct', 'gb-wls', 'gd', 'ge', 'gf', 'gg', 'gh',
+    'gi', 'gl', 'gm', 'gn', 'gp', 'gq', 'gr', 'gs', 'gt', 'gu', 'gw', 'gy', 'hk', 'hm', 'hn',
+    'hr', 'ht', 'hu', 'id', 'ie', 'il', 'im', 'in', 'io', 'iq', 'ir', 'is', 'it', 'je', 'jm',
+    'jo', 'jp', 'ke', 'kg', 'kh', 'ki', 'km', 'kn', 'kp', 'kr', 'kw', 'ky', 'kz', 'la', 'lb',
+    'lc', 'li', 'lk', 'lr', 'ls', 'lt', 'lu', 'lv', 'ly', 'ma', 'mc', 'md', 'me', 'mf', 'mg',
+    'mh', 'mk', 'ml', 'mm', 'mn', 'mo', 'mp', 'mq', 'mr', 'ms', 'mt', 'mu', 'mv', 'mw', 'mx',
+    'my', 'mz', 'na', 'nc', 'ne', 'nf', 'ng', 'ni', 'nl', 'no', 'np', 'nr', 'nu', 'nz', 'om',
+    'pa', 'pe', 'pf', 'pg', 'ph', 'pk', 'pl', 'pm', 'pn', 'pr', 'ps', 'pt', 'pw', 'py', 'qa',
+    're', 'ro', 'rs', 'ru', 'rw', 'sa', 'sb', 'sc', 'sd', 'se', 'sg', 'sh', 'si', 'sj', 'sk',
+    'sl', 'sm', 'sn', 'so', 'sr', 'ss', 'st', 'sv', 'sx', 'sy', 'sz', 'tc', 'td', 'tf', 'tg',
+    'th', 'tj', 'tk', 'tl', 'tm', 'tn', 'to', 'tr', 'tt', 'tv', 'tw', 'tz', 'ua', 'ug', 'um',
+    'us', 'uy', 'uz', 'va', 'vc', 've', 'vg', 'vi', 'vn', 'vu', 'wf', 'ws', 'xk', 'ye', 'yt',
+    'za', 'zm', 'zw', 'ad', 'ae'
+  ];
+
+  // Bayroqni SVG formatda olish va o'lchamlarni parametr sifatida olish
+  static Widget getFlag(String countryCode, {double width = 50.0, double height = 50.0}) {
+    // Davlat kodi ro'yxatini tekshirish
+    if (!_validCountryCodes.contains(countryCode.toLowerCase())) {
+      return _errorWidget('Error code : $countryCode');
+    }
+
+    // Har bir davlat kodi bo'yicha SVG faylni olish
+    String flagAsset = 'assets/svg/$countryCode.svg';
+
+    // Bayroqni ko'rsatish
+    return SvgPicture.asset(
+      flagAsset,
+      width: width,
+      height: height,
+      placeholderBuilder: (BuildContext context) => CircularProgressIndicator(), // Loading indikator
+    );
+  }
+
+  // Xatolikni ko'rsatish uchun yordamchi widget
+  static Widget _errorWidget(String errorMessage) {
+    return Center(
+      child: Text(
+        errorMessage,
+        style: TextStyle(color: Colors.red, fontSize: 16),
+      ),
+    );
+  }
 }
--- test/country_flags_pro_test.dart
+++ test/country_flags_pro_test.dart
@@ -1,12 +1,40 @@
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:country_flags_pro/country_flags_pro.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_svg/flutter_svg.dart';// CountryFlag sinfini import qilasiz
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('CountryFlag Widget Tests', () {
+    testWidgets('Valid country code flag is displayed correctly', (WidgetTester tester) async {
+      // Test uchun to'g'ri davlat kodi ('us') ni kiriting
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: CountryFlagsPro.getFlag('us', width: 100, height: 100),
+        ),
+      ));
+
+      // SVG bayroqning to'g'ri yuklangani va o'lchamlari tekshirilmoqda
+      final svgFinder = find.byType(SvgPicture);
+
+      expect(svgFinder, findsOneWidget);
+      
+      final svgWidget = tester.widget<SvgPicture>(svgFinder);
+      expect(svgWidget.width, 100);
+      expect(svgWidget.height, 100);
+    });
+
+    testWidgets('Invalid country code shows error message', (WidgetTester tester) async {
+      // Test uchun noto\'g\'ri davlat kodi ('xx') ni kiriting
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: CountryFlagsPro.getFlag('xx'),
+        ),
+      ));
+
+      // Xatolik xabari ko'rsatilishini tekshirish
+      final errorMessageFinder = find.text('Error code : xx');
+
+      expect(errorMessageFinder, findsOneWidget);
+    });
   });
 }
